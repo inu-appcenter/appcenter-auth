dbff17ce8b20d65325b25e5f184873334baecdc0
DESKTOP-TA2I9Q2\limso <limson_95@naver.com>
2019-05-18 17:06:41 +0900

    apply ignore

--- routes/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secret' : 'InUaPpCeNtEr',
-    'jwt-secret' : 'ApPcEnTeRaCcOuNt',
-    'mongodbUri' : 'mongodb://localhost/INUM',
-    'mailId' : 'inuappcenter@gmail.com',
-    'mailPasswd' : 'inuappcenter110825',
-    'verifyPath' : 'http://117.16.231.66:1337/verified/'
-}
